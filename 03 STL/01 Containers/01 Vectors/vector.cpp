11085bb64e5d5abe0e2b01adb64e97f4b3397f20
hustlerZzZ <rishabpal2000@gmail.com>
2024-06-09 12:17:25 +0530

    Added List

--- 03 STL/01 Containers/01 Vectors/vector.cpp
+++ 03 STL/01 Containers/01 Vectors/vector.cpp
@@ -5,17 +5,39 @@ using namespace std;
 
 int main()
 {
+    vector<vector<int>> arr(4);
+    arr[0] = vector<int>(4);
+    arr[1] = vector<int>(2);
+    arr[2] = vector<int>(5);
+    arr[3] = vector<int>(3);
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = 0; j < arr[i].size(); j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // vector<vector<int>> arr(5, vector<int>(4, 0));
+
     // Creation
-    vector<int> marks;
+    // vector<int> marks;
 
-    marks.push_back(10);
-    marks.push_back(20);
-    marks.push_back(30);
-    marks.push_back(40);
-    marks.push_back(40);
+    // marks.push_back(10);
+    // marks.push_back(20);
+    // marks.push_back(30);
+    // marks.push_back(40);
+    // marks.push_back(40);
 
-    cout << marks[0] << endl;
-    cout << marks.capacity() << endl;
+    // vector<int>::iterator it = marks.begin();
+
+    // while (it != marks.end())
+    // {
+    //     cout << *it << " " << endl;
+    //     it++;
+    // }
 
     // cout << "Size:" << marks.size() << endl;
 
---
+++ 03 STL/01 Containers/02 Lists/List.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+    list<int> myList;
+
+    return 0;
+}
